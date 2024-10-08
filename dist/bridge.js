9f317716ff21500d30357296d77ba7626a3e9a70
asissuthar <asissuthar@gmail.com>
2024-10-08 14:21:31 +0530

    Version Sync

--- dist/bridge.js
+++ dist/bridge.js
@@ -76,7 +76,7 @@ class w {
     return (e = window.bridge) == null ? void 0 : e.version();
   }
   version() {
-    return "4.0.6";
+    return "4.0.7";
   }
   async ready(e = h) {
     if (this.bridgeIsReady) return null;
