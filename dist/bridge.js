45b1ffa5d4a648358c0588044c9aedfb21b1c915
asissuthar <asissuthar@gmail.com>
2024-10-08 18:06:28 +0530

    v4.0.10

--- dist/bridge.js
+++ dist/bridge.js
@@ -76,7 +76,7 @@ class w {
     return (e = window.bridge) == null ? void 0 : e.version();
   }
   version() {
-    return "4.0.9";
+    return "4.0.10";
   }
   async ready(e = h) {
     if (this.bridgeIsReady) return null;
